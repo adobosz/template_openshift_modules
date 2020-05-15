71a338e3d171b08286f77a0033e9276a9cf5752b
adobosz <andrea.dobosz@gmail.com>
2020-05-15 16:34:46 +0200

    Update variables.tf

--- config_inventory/variables.tf
+++ config_inventory/variables.tf
@@ -1,6 +1,5 @@
 variable "vm_os_password"       { type = "string"  description = "Operating System Password for the Operating System User to access virtual machine"}
 variable "vm_os_user"           { type = "string"  description = "Operating System user for the Operating System User to access virtual machine"}
-variable "private_key"          { type = "string"  description = "Private SSH key Details to the Virtual machine"}
 variable "master_node_ip"       { type = "string"}
 variable "master_node_hostname"           { type = "string"}
 variable "etcd_node_hostname"             { type = "string"}
@@ -13,5 +12,3 @@ variable "infra_node_ip"         { type = "string"}
 variable "rh_user"              { type = "string" }
 variable "rh_password"          { type = "string" }
 variable "compute_node_enable_glusterfs"          { type = "string"}
-variable "random"               { type = "string"  description = "Random String Generated"}
-variable "dependsOn"            { default = "true" description = "Boolean for dependency"}
